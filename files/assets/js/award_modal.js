31f5ada1d14e35f56ce238b60344989dc309a31d
TLSM <duolsm@outlook.com>
2022-05-19 13:56:04 -0400

    Award modal: flairlock note length limit in UI.
    The award modal note field had a maxLength of 200 characters.
    This was retained when repurposing the field for the new flair to be
    applied by a flairlock award. However, flairs have a maxLength of 100
    characters. award_modal.js now makes this clearer in the UI by
    changing the maxLength of note when a flairlock award is picked.

--- files/assets/js/award_modal.js
+++ files/assets/js/award_modal.js
@@ -102,11 +102,13 @@ function pick(kind, canbuy1, canbuy2) {
 	document.getElementById(kind).classList.toggle('picked')
 	if (kind == "flairlock") {
 		document.getElementById('notelabel').innerHTML = "New flair:";
-		document.getElementById('note').placeholder = "Insert new flair here, or leave empty to add 1 day to the duration of the current flair";
+		document.getElementById('note').placeholder = "Insert new flair here, or leave empty to add 1 day to the duration of the current flair. 100 characters max.";
+		document.getElementById('note').maxLength = 100;
 	}
 	else {
 		document.getElementById('notelabel').innerHTML = "Note (optional):";
 		document.getElementById('note').placeholder = "Note to include in award notification";
+		document.getElementById('note').maxLength = 200;
 	}
 }
 
