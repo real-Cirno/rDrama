e96539abacf4e537f5a46feb88ec9c450438a58c
Outrun Colors, LLC <itsoutruncolors@gmail.com>
2022-05-28 18:26:30 -0500

    Initial connection of front-end to back-end.

--- files/assets/js/lottery_modal.js
+++ files/assets/js/lottery_modal.js
@@ -8,3 +8,47 @@ window.addEventListener("DOMContentLoaded", () => {
     }, 1600);
   });
 });
+
+function purchaseLotteryTicket() {
+  const xhr = new XMLHttpRequest();
+  const url = `/lottery/buy`;
+
+  xhr.open("POST", url);
+  xhr.setRequestHeader("xhr", "xhr");
+  xhr.onload = function () {
+    let response;
+
+    try {
+      response = JSON.parse(xhr.response);
+    } catch (error) {
+      console.error(error);
+    }
+
+    const succeeded =
+      xhr.status >= 200 && xhr.status < 300 && response && response.message;
+
+    if (succeeded) {
+      // Display success.
+      const toast = document.getElementById("lottery-post-success");
+      const toastMessage = document.getElementById("lottery-post-success-text");
+
+      toastMessage.innerText = response.message;
+
+      bootstrap.Toast.getOrCreateInstance(toast).show();
+    } else {
+      // Display error.
+      const toast = document.getElementById("lottery-post-error");
+      const toastMessage = document.getElementById("lottery-post-error-text");
+
+      toastMessage.innerText =
+        (response && response.error) || "Error, please try again later.";
+
+      bootstrap.Toast.getOrCreateInstance(toast).show();
+    }
+  };
+
+  const form = new FormData();
+  form.append("formkey", formkey());
+
+  xhr.send(form);
+}
