488b2213d4dbc319b30b51e437134d4bbca248f2
Aevann1 <randomname42029@gmail.com>
2022-06-23 23:25:07 +0200

    fix this https://chapotraphouse.club/post/18459/marseycapywalking-megathread-for-bugs-and-suggestions/2130416?context=8#context

--- files/assets/js/comments.js
+++ files/assets/js/comments.js
@@ -1,3 +1,24 @@
+function poll_vote_no_v() {
+	document.getElementById('toast-post-error-text').innerText = "Only logged-in users can vote!";
+	bootstrap.Toast.getOrCreateInstance(document.getElementById('toast-post-error')).show();
+}
+
+function expandMarkdown(id,type) {
+	const ta = document.getElementById('markdown-'+id);
+	ta.classList.toggle('d-none');
+	autoExpand(ta);
+	document.getElementsByClassName('text-expand-icon-'+id)[0].classList.toggle('fa-expand-alt');
+	document.getElementsByClassName('text-expand-icon-'+id)[0].classList.toggle('fa-compress-alt');
+
+	const items = document.getElementsByClassName(`expand-text-${type}-${id}`)
+	for (let i=0; i < items.length; i++)
+	{	
+		const e = items[i]
+		if (e.innerHTML == 'View source') e.innerHTML = 'Hide source'
+		else e.innerHTML = 'View source'
+	}
+};
+
 function collapse_comment(id, element) {
 	const closed = element.classList.toggle("collapsed")
 	const top = element.getBoundingClientRect().y
@@ -15,21 +36,8 @@ function collapse_comment(id, element) {
 	{
 		vids[i].pause()
 	}
-};
 
-function poll_vote_no_v() {
-	document.getElementById('toast-post-error-text').innerText = "Only logged-in users can vote!";
-	bootstrap.Toast.getOrCreateInstance(document.getElementById('toast-post-error')).show();
-}
-
-function expandMarkdown(t,id) {
-	let ta = document.getElementById('markdown-'+id);
-	ta.classList.toggle('d-none');
-	autoExpand(ta);
-	document.getElementsByClassName('text-expand-icon-'+id)[0].classList.toggle('fa-expand-alt');
-	document.getElementsByClassName('text-expand-icon-'+id)[0].classList.toggle('fa-compress-alt');
-
-	let val = t.getElementsByTagName('span')[0]
-	if (val.innerHTML == 'View source') val.innerHTML = 'Hide source'
-	else val.innerHTML = 'View source'
+	const ta = document.getElementById('markdown-'+id);
+	if (!ta.classList.contains('d-none'))
+		expandMarkdown(id,'c')
 };
