d207d7cf834cf4f0e257d958198793e2382afac3
Aevann1 <randomname42029@gmail.com>
2022-06-24 03:06:13 +0200

    don't create a modal instance if one exists

--- files/assets/js/settings_security.js
+++ files/assets/js/settings_security.js
@@ -3,7 +3,7 @@ document.getElementById('new_email').addEventListener('input', function () {
 	document.getElementById("email-password-label").classList.remove("d-none");
 	document.getElementById("emailpasswordRequired").classList.remove("d-none");
 });
-const twoStepModal = new bootstrap.Modal(document.getElementById('2faModal'))
+const twoStepModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('2faModal'))
 
 function emailVerifyText() {
 	document.getElementById("email-verify-text").innerHTML = "Verification email sent! Please check your inbox.";
