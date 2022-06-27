f173d099b05b2c420417c145a70a241211aba9be
Aevann1 <randomname42029@gmail.com>
2022-06-27 03:00:45 +0200

    embed rework

--- files/assets/js/submission_listing.js
+++ files/assets/js/submission_listing.js
@@ -8,9 +8,21 @@ function expandText(id) {
 };
 
 function togglevideo(pid) {
+	const e = this.event
+	if(e.ctrlKey || e.metaKey || e.shiftKey || e.altKey)
+		return true;
+	e.preventDefault();
 	let vid = document.getElementById(`video-${pid}`).classList
 	vid.toggle('d-none')
 	let vid2 = document.getElementById(`video2-${pid}`)
 	if (vid.contains('d-none')) vid2.pause()
 	else vid2.play()
+}
+
+function toggleyoutube(pid) {
+	const e = this.event
+	if(e.ctrlKey || e.metaKey || e.shiftKey || e.altKey)
+		return true;
+	e.preventDefault();
+	document.getElementById(`video-${pid}`).classList.toggle('d-none')
 }
